ed294f1d1bd1814ccfad95d40ff8ca334a99af85
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-05-28 13:30:51 +0300

    finish 7hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+const markapEl = galleryItems.map(({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+});
+galleryEl.insertAdjacentHTML("afterbegin", markapEl.join(""));
+
+function openModal(original) {
+  const instance = basicLightbox.create(`
+  <img src="${original}" width="800" height="600" >
+  `);
+  
+  instance.show();
+  
+  window.addEventListener("keydown", (e) => {
+    if (e.key !== "Escape") {
+      return;
+    }
+    instance.close();
+  });
+}
+
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault()
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  openModal(e.target.dataset.source);
+});
+// galleryEl.addEventListener('click', e => {
+//   e.preventDefault();
+//   const { target } = e;
+//   if (target.nodeName !== 'IMG') {
+//     return;
+//   }
+//   openModal(target.dataset.source);
+// })
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+const markapGallery = galleryItems.map(({preview, original, description}) => {
+  return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+
+
+    `;
+});
+
+galleryEl.insertAdjacentHTML('afterbegin', markapGallery.join(''))
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+
+ });
